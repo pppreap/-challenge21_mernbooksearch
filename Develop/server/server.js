b06048ef8aa7f529447f7602b9687d1ecc46b405
pppreap <paulap@Paulas-MacBook-Pro.local>
2022-10-31 15:38:16 -0400

    edit server file to import apollo express and auth middleware and set server with typdef and resolvers

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-epxress');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +25,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
+app.get('*', (req, res)=> {
+  res.sendFile(path.join(_dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () =>{
+   console.log(`🌍 API server running on port:${PORT}`);
+   console.log (`Use GraphQL at  http://localhost:${PORT}${server.graphqlPath}`);
+});
 });
